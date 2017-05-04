6d17e91fdaa0e15b97a477e88cdb5ee4cf362340
rocket <rocket@4282536d-2d76-4d18-87bf-2ecf27506aab>
2017-05-04 05:11:23 +0000

    修改即墨txt 导出  每个字段的长度为字节数   汉字按照gbk为2个字节
    git-svn-id: https://desktop-ee61sbk:8090/svn/remoteMeter/yccb/remoteMeter@242 4282536d-2d76-4d18-87bf-2ecf27506aab

--- src/com/xdkj/yccb/main/readme/export/impl/JM_4_txt_ExportReadImpl.java
+++ src/com/xdkj/yccb/main/readme/export/impl/JM_4_txt_ExportReadImpl.java
@@ -1,6 +1,7 @@
 package com.xdkj.yccb.main.readme.export.impl;
 
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -42,14 +43,22 @@ public class JM_4_txt_ExportReadImpl implements ExportRead {
 			String dfyf = new SimpleDateFormat("yyyyMM").format(new Date());
 			for(int i =0;i<list.size();i++){
 				view = list.get(i);
-				String line = String.format("%-20s", view.getM_apid())+
-						String.format("%-50s", view.getCustomerAddr())+
-						String.format("%-50s", view.getCustomerName())+
-						String.format("%-10s", dfyf)+
-						String.format("%-10s", "0")+
-						String.format("%-10s", view.getReaddata())+
-						String.format("%-15s", view.getCustomerMobile())+
-						String.format("%-10s", view.getN_remark());
+//				String line = String.format("%-20s", view.getM_apid())+
+//						String.format("%-50s", view.getCustomerAddr())+
+//						String.format("%-50s", view.getCustomerName())+
+//						String.format("%-10s", dfyf)+
+//						String.format("%-10s", "0")+
+//						String.format("%-10s", view.getReaddata())+
+//						String.format("%-15s", view.getCustomerMobile())+
+//						String.format("%-10s", view.getN_remark());
+				String line = appendToLength(view.getM_apid(),20)+
+						appendToLength(view.getCustomerAddr(),50)+
+						appendToLength(view.getCustomerName(),50)+
+						appendToLength(dfyf,10)+
+						appendToLength("0",10)+
+						appendToLength(view.getReaddata()+"",10)+
+						appendToLength(view.getCustomerMobile()+"",15)+
+						appendToLength(view.getN_remark()+"",10);
 				//数据库中小区的备注表示   txt导出时对应的本号
 				
 				if(i != list.size()-1){
@@ -64,6 +73,27 @@ public class JM_4_txt_ExportReadImpl implements ExportRead {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * 将string按照固定的字节数输出  格式为gbk  汉字为2个字节
+	 * @param ori
+	 * @param length
+	 * @return
+	 */
+	public String appendToLength(String ori,int length){
+		String append = ori;
+		try {
+			byte[] oribytes = ori.getBytes("gbk");
+			
+			int appendlen = length - oribytes.length;
+			for(int i = 0;i < appendlen;i++){
+				append += " ";
+			}
+		} catch (UnsupportedEncodingException e) {
+		}
+		
+		return append;
 	}
 
 }
